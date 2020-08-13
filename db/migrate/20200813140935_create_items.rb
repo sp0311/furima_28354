18d3717aa22876550d48f3ccadfbfea166eec4e1
sp0311 <qploop@yahoo.co.jp>
2020-08-14 02:36:42 +0900

    トップページ表示

---
+++ db/migrate/20200813140935_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name, null:false
+      t.references :user, null: false
+      t.text :introduction, null: false, foreign_key: true
+      t.integer :price, null: false
+      t.integer :category, null: false
+      t.integer :item_condition, null: false
+      t.integer :pastage_payer, null: false
+      t.integer :preparation_day, null: false
+      t.integer :pastage_type, null: false
+      t.string :image, null: false
+      t.integer :prefecture_code, null: false
+    end
+  end
+end
+ 
