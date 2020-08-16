b30a59e293dc0468e78c4018ac69c006177545b5
sp0311 <qploop@yahoo.co.jp>
2020-08-16 22:07:56 +0900

    first commit
    ・Userのマイグレーション作成
    ・ApplicationController のdevise用ストロングパラメータ作成
    ・ビューのプロパティ名をDBのカラム名と合わせる
    ・RegistrationsControllerのcreateアクションでsave
    createアクションに
    ・userを保存
    ・保存した後にトップページへ
    ・itemsテーブル作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
       username == 'admin' && password == '2222'
     end
   end
+
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :family_name, :first_name_kana, :family_name_kana, :nickname, :birth_day])
+  end
 end
