0aafadc9958368deb210ae0294908af4cac83ab3
sp0311 <qploop@yahoo.co.jp>
2020-08-22 05:51:52 +0900

    activehash作成(カテゴリー以外)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,13 +8,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def create
-    Item.create(item_params)
+    # Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  private 
+  private
   def item_params
-  params.require(:item).permit(:image, :text)
+  params.require(:item).permit(:name, :introduction,:price, :category,:item_condition, :postage_payer, :preparation_day,:postage_type, :image, :prefecture_code)
   end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,16 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :preparation_day
+  belongs_to_active_hash :prefecture_code
 
   validates :name, :user, :introduction,:price, :category,
             :item_condition, :postage_payer, :preparation_day,
             :postage_type, :image, :prefecture_code,
             presence: true
 
-  validates :category_id, numericality: { other_than:1 }
+  validates :category_id, :item_condition_id, :postage_payer_id, :preparation_day_id, :prefecture_code,
+            numericality: { other_than: 1 }
 end
