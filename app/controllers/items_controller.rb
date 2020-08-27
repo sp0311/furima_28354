40ea972727b0e1a6bfc4aba8b3eb311dc5419076
sp0311 <qploop@yahoo.co.jp>
2020-08-27 18:23:18 +0900

    destroy if else

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :new, :show]
   before_action :set_item, except:[:index, :new, :create]
  
-
-  
   
   def index
     @items = Item.all
@@ -22,24 +20,23 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-    
+  def edit 
   end
 
   def update
-   
     @item.update(item_params)
     redirect_to root_path
   end
 
   def destroy
-    
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render 'show'
+    end
   end
 
   def show
-    
   end
 
   private
@@ -48,11 +45,8 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-
-
-
   def item_params
     params.require(:item).permit(:name, :introduction, :price, :category_id, :item_condition_id, :postage_payer_id, :preparation_day_id, :image, :prefecture_code_id)
           .merge(user_id: current_user.id)
-end
+  end
 end
