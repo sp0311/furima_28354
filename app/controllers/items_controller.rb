f84eb27e9d003108a63c646ccc28b123f42a4672
sp0311 <qploop@yahoo.co.jp>
2020-08-27 17:38:16 +0900

    before_action追加・不要部分削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,12 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :new, :show]
+  before_action :set_item, except:[:index, :new, :create]
+ 
 
+  
+  
   def index
     @items = Item.all
-    # order("created_at DESC")
   end
 
   def new
@@ -20,25 +23,33 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+   
+    @item.update(item_params)
+    redirect_to root_path
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    
+    @item.destroy
     redirect_to root_path
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+
+
 
   def item_params
     params.require(:item).permit(:name, :introduction, :price, :category_id, :item_condition_id, :postage_payer_id, :preparation_day_id, :image, :prefecture_code_id)
